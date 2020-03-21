7ba5ee70de27789ca8a9689158b61424b9eea43c
Shota Kato <sheeptech@vivaldi.net>
2020-03-21 22:31:25 +0900

    コンテンツを追加

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '山口研究室',
+  description: '千葉工業大学 山口研究室',
+  themeConfig: {
+    nav: [
+      { text: 'ホーム', link: '/' },
+      { text: '研究室紹介', link: '/about/' },
+      { text: 'メンバー', link: '/member/' },
+      { text: '環境', link: '/environment/' },
+      { text: 'スケジュール', link: '/schedule/' },
+      { text: '千葉工業大学', link: 'https://www.it-chiba.ac.jp/' },
+      { text: '千葉工業大学情報工学科', link: 'http://www.cs.it-chiba.ac.jp/' },
+    ],
+    search: false
+  }
+}
